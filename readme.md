763e06f5dc03775ffdb831e8597658bc60158c96
Johnnie <46320367+JohnnieDK@users.noreply.github.com>
2020-07-07 16:26:14 +0200

    created github pipeline to check for new version automatically

--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
-﻿This container was created to keep running in the background.  
+This container was created to keep running in the background.  
 Remote into it, and download the video you specify.  
 Youtube-dl will then choose the best mp4 it can find and merge it with the best audio (usually up to 1080p).  
 And download the file to your drive that is mapped (in thise case on windows c:\temp). 
@@ -26,4 +26,4 @@ You can download the container on [dockerhub](https://hub.docker.com/r/bladedk/y
 
 [youtube-dl](https://yt-dl.org)
 
-vers: youtube-dl 2020.06.06
+vers: youtube-dl 2020.06.16.1
--- scripts/checkvers.ps1
+++ scripts/checkvers.ps1
@@ -3,25 +3,19 @@
 # update readme.md and commit the file
 ##########################################################
 
-[CmdletBinding()]
-param (
-    [Parameter()]
-    [switch] $forcepush
-)
-
-$fileHistory = "history.json"
-$fileNameReadme = "readme.md"
-$fileReadme = (Join-Path -Path ".." -ChildPath $fileNameReadme  -Resolve)
+$fileNameHistory = "history.json"
+$fileNameReadme  = "readme.md"
+$fileReadme      = (Join-Path -Path "." -ChildPath $fileNameReadme  -Resolve)
+$fileHistory     = (Join-Path -Path "scripts" -ChildPath $fileNameHistory  -Resolve)
 
 $url = "https://api.github.com/repos/ytdl-org/youtube-dl/releases"
 
 $response = Invoke-WebRequest -UseBasicParsing -Uri $url 
+$json     = ($response.Content | ConvertFrom-Json)[0]
 
-$json = ($response.Content |ConvertFrom-Json)[0]
+$history = Get-Content $fileHistory | ConvertFrom-Json
 
-$history = Get-Content $fileHistory |ConvertFrom-Json
-
-if ($json.created_at -lt $history.created_at) { 
+if ($json.created_at -le $history.created_at) { 
     return  # same version, do nothing
 }
 Write-Host "*** Newer version found ***" -BackgroundColor Cyan -ForegroundColor Black
@@ -35,19 +29,9 @@ $history = @{"tag_name"    = $json.tag_name
              "name"        = $json.name
              "created_at"  = $json.created_at
 }
-
 $history | ConvertTo-Json | Out-File $fileHistory
 
-$gitMessage = "Bumped version to $($json.name)"
-
-git add $fileReadme
-git commit -m $gitMessage
-
-if ($forcepush.IsPresent) {
-    git push
-} else {
-    Write-Host "******************************" -ForegroundColor Yellow
-    Write-Host $gitMessage -ForegroundColor Yellow
-    Write-Host "Dont forget to push" -ForegroundColor Yellow
-    Write-Host "******************************" -ForegroundColor Yellow
-}
+Write-Host "******************************" -ForegroundColor Yellow
+Write-Host $gitMessage -ForegroundColor Yellow
+Write-Host "Dont forget to push" -ForegroundColor Yellow
+Write-Host "******************************" -ForegroundColor Yellow
